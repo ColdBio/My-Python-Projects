d4d1800fa8a7d9b084682692dfa1eb6355fa203b
ColdBio <64978825+ColdBio@users.noreply.github.com>
2021-08-30 17:38:01 +0100

    Create SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,19 @@
+"""
+A simple python calculator
+""" 
+def initial_values():
+    print("Enter a value")
+    a = int(input())
+    print("Enter another value")
+    b = int(input())
+    print(f"The values you entered are {a} and {b}")
+    print("Please enter the type of operation you would like to perform \n for example '+' denotes the addition operation")
+    global op 
+    op = input()
+    try:
+        print(eval(f"a {op} b"))
+    except:
+        print("🚫 An error has occured, please run program again 🚫")
+
+
+initial_values()
