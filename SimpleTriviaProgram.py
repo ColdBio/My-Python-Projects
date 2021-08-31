db31dde7c4b0d58d7033e47f30e7819921110198
ColdBio <64978825+ColdBio@users.noreply.github.com>
2021-08-31 14:33:53 +0100

    Create SimpleTriviaProgram.py

---
+++ SimpleTriviaProgram.py
@@ -0,0 +1,40 @@
+# The code below fetches data from an api that provides trivia related information and displays it in a simple GUI. 
+# The api can be viewed from here:- https://opentdb.com/api_config.php
+
+import json
+import requests
+import tkinter as tk
+import html
+
+# Accessing API data
+response = requests.get("https://opentdb.com/api.php?amount=1")
+data = json.loads(response.text)
+
+# Making sure to only deal with the needed data
+for each in data["results"]:
+    category = html.unescape(each["category"])
+    question = html.unescape(each["question"])
+    answer = html.unescape(each["correct_answer"])
+    difficulty = html.unescape(each["difficulty"])
+
+# GUI
+window = tk.Tk()
+window.title("❌ Simple Trivia ✅")
+
+labelC = tk.Label(text=f"Category: {category}")
+labelQ = tk.Label(text=f"Qesution: {question}")
+labelA = tk.Label(text=f"Answer: {answer}")
+
+# Based on difficulty a visual cue is displayed
+if difficulty == "hard":
+    labelD = tk.Label(text=f"Difficulty: {difficulty}", fg="red")
+elif difficulty == "medium":
+     labelD = tk.Label(text=f"Difficulty: {difficulty}", fg="yellow")
+elif difficulty == "easy":
+     labelD = tk.Label(text=f"Difficulty: {difficulty}", fg="green")
+
+labelC.pack()
+labelD.pack()
+labelQ.pack()
+labelA.pack()
+window.mainloop()
