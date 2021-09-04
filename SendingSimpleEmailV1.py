3a53922f92ee1b8ac0035b2a82881e380e63b998
ColdBio <64978825+ColdBio@users.noreply.github.com>
2021-09-04 21:26:12 +0100

    Create SendingSimpleEmailV1.py

---
+++ SendingSimpleEmailV1.py
@@ -0,0 +1,29 @@
+import smtplib, ssl
+import getpass
+
+"""
+This project was completed as a code along using this guide -> https://realpython.com/python-send-email/
+
+TODOs:- 
+1. Create a simple function that takes in a user defined sender, receiver, subject, and body using the terminal
+2. Allow someone to send emails using a GUI
+"""
+
+# Sender and Receiver Emails
+sender = "throwawaynon54321@gmail.com"
+receiver = "throwawaynon54321@gmail.com"
+message = 'Subject: Hello World :D' + '\n\n' + 'Lorem Ipsum'
+
+
+# Gmail requires that port 465 be used if connecting using SMTP_SSL
+port = 465
+
+# Creating a secure SSL context
+context = ssl.create_default_context()
+
+# Prevents the password being stored within the code
+password = getpass.getpass()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(sender, password)
+    server.sendmail(sender, receiver, message)
