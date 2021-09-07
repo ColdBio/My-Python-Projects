f76046b05affd8e127d431c83609a71ed78ba138
ColdBio <yunis@Yuniss-MacBook-Pro.local>
2021-09-07 14:00:40 +0100

    Added search label, text field, and button for GUI

--- BasicWeatherAppV1.py
+++ BasicWeatherAppV1.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageTk
 
 def weather_api():
 
-    url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid={Add your own API key here}")
+    url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid={Replace with your keys}")
     data = json.loads(url.text)
 
     city_name = data["name"]
@@ -61,6 +61,12 @@ def GUI():
     temp_min_max_label = tk.Label(text=f"min: {round(temp_min, 1)}°C      max: {round(temp_max, 1)}°C")
     temp_min_max_label.config(font=("Calibri", 14))
 
+    search_city_label = tk.Label(text="City")
+    search_city_field = tk.Entry(text="", width= 62)
+
+
+    submit_button = tk.Button(window, text="Search", width= 62)
+
 
     # Dealinng with the Image icon
     image_data = image_extractor(icon_url)
@@ -76,6 +82,11 @@ def GUI():
     temp_label.grid(row=3, column=2)
     description_label.grid(row=4, column=2)
     temp_min_max_label.grid(row=5, column=2)
+
+    search_city_field.grid(row=6, column=2)
+    search_city_label.grid(row=6, column=1)
+
+    submit_button.grid(row=7, column=2)
 
     window.mainloop()
 
