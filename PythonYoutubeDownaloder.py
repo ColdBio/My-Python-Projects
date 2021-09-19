ea8a12198e7754414d0242a32785c4925b3d5de8
ColdBio <64978825+ColdBio@users.noreply.github.com>
2021-09-19 21:53:41 +0100

    Create PythonYoutubeDownaloder.py

---
+++ PythonYoutubeDownaloder.py
@@ -0,0 +1,84 @@
+import tkinter as tk
+from tkinter import ttk
+from pytube import YouTube
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import io
+import urllib.request
+from io import BytesIO
+
+
+
+all_res = ["240p", "360p", "480p", "720p", "1080p", "1440p", "2160p"]
+
+def get_url_image(data):
+    image = urllib.request.urlopen(data)
+    img_data = image.read()
+    return img_data
+
+
+class Python_Video_Downloader:
+
+
+
+
+    def GUI():
+        window = tk.Tk()
+        window.title("🐍 Python Youtube Downloader ▶️")
+
+        link_label = tk.Label(text="Video URL: ")
+        link_entry = tk.Entry(text="", width=40)
+        download_path_label = tk.Label(text="Download Path: ")
+        download_path = tk.Entry(text="", width=40)
+        video_title = tk.Label(text="Video Title: ")
+        select_res = ttk.Combobox(window, values=all_res)
+        select_res.current(0)
+
+        def find_video():
+            user_link = link_entry.get()
+            try:
+                yt = YouTube(user_link)
+                print(yt.title)
+                print(yt.thumbnail_url)
+                yt_thumbnail = get_url_image(yt.thumbnail_url)
+
+                im = Image.open(BytesIO(yt_thumbnail))
+                width, height = im.width, im.height
+
+                im = im.resize((int(width / 5), int(height / 5)))
+                image = ImageTk.PhotoImage(im)
+                print(image)
+
+
+                video_title['text'] = "Video Title: " + yt.title       
+                print(select_res.get())       
+
+            except Exception as e:
+                print(e)
+                messagebox.showerror("Error", f"Invalid URL link")
+
+        def download_selected_video():
+            yt = YouTube(f"{link_entry.get()}")
+            stream_list = yt.streams.filter(file_extension='mp4', res=f"{select_res.get()}")
+            for each in stream_list:
+                try:
+                    each.download(output_path=f"{download_path.get()}")
+                except Exception as e:
+                    print(f"[!] Error {e}")
+
+        find_btn = tk.Button(text="Find Video", command=find_video)
+        download_btn = tk.Button(text=" Download Video", command=download_selected_video)
+
+
+        link_label.grid(row=1, column=1)
+        link_entry.grid(row=1, column=2)
+        find_btn.grid(row=1, column=3)
+        video_title.grid(row=2, column=2)
+        select_res.grid(row=2, column=3)
+        download_path_label.grid(row=4, column=1)
+        download_path.grid(row=4, column=2)
+        download_btn.grid(row=4, column=3)
+        window.mainloop()
+
+
+Python_Video_Downloader.GUI()
