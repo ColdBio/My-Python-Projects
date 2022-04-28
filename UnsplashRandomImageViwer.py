2ae7381d3db0df71d5acc646bb3d064fdf7e8142
ColdBio <64978825+ColdBio@users.noreply.github.com>
2022-04-28 01:18:35 +0100

    Update UnsplashRandomImageViwer.py

--- UnsplashRandomImageViwer.py
+++ UnsplashRandomImageViwer.py
@@ -1,101 +1,96 @@
 import tkinter as tk
 from tkinter import ttk
-import json
-import requests        
+from pytube import YouTube
+from tqdm import tqdm
+from tkinter import messagebox
 from PIL import Image, ImageTk
 import io
 import urllib.request
 from io import BytesIO
-import string
+
+"""
+Credit to Pytube for providing a lightweight, dependency-free Python library (and command-line utility) for downloading YouTube Videos.
+Github: https://github.com/pytube/pytube
+Documentation: https://pytube.io/en/latest/
+
+"""
 
 
-window = tk.Tk()
-window.title("Unsplash Random Image Viewer")
+all_res = ["240p", "360p", "480p", "720p", "1080p", "1440p", "2160p"]
 
-
-# You can change the number of images returned by altering the 'count' to a desired number
-# Keep in mind that the API only allows 50 calls per day
-url = "https://api.unsplash.com/photos/random?client_id={Replace with your own API key}&count=10"
-url = requests.get(url)
-data = json.loads(url.text)
-
-image_list = []
-desc_list = []
-username_list = []
-likes_list = []
-downloads_list = []
-views_list = []
-
-
-for each in data:
-    image_list.append(each["urls"]["regular"])
-
-for each in data:
-    desc_list.append(each["alt_description"])
-
-for each in data:
-    username_list.append(each["user"]["username"])
-
-for each in data:
-    likes_list.append(each["likes"])
-
-for each in data:
-    downloads_list.append(each["downloads"])
-
-for each in data:
-    views_list.append(each["views"])
-
-def get_url_image(some_input):
-    image = urllib.request.urlopen(some_input)
+def get_url_image(data):
+    image = urllib.request.urlopen(data)
     img_data = image.read()
-    im = Image.open(BytesIO(img_data))
-    width, height = im.width, im.height
-    im = im.resize((int(width / 5), int(height / 5)))
-    image = ImageTk.PhotoImage(im)
-    return image
-
-images = []
-
-for each in image_list:
-    images.append(get_url_image(each))
-
-img_iter = iter(images)
-desc_iter = iter(desc_list)
-username_iter = iter(username_list)
-likes_iter = iter(likes_list)
-downloads_iter = iter(downloads_list)
-views_iter = iter(views_list)
-
-image = tk.Label(image=next(img_iter))
-alt_desc = tk.Label(text=next(desc_iter))
-
-username = tk.Label(text="Username: " + next(username_iter))
-likes = tk.Label(text=f"Likes: {next(likes_iter)}")
-downalods = tk.Label(text=f"Downalods: {next(downloads_iter)}")
-views = tk.Label(text=f"Views: {next(views_iter)}")
-
-image.grid(row=1, column=1)
-alt_desc.grid(row=2, column=1)
-
-username.grid(row=3, column=1)
-likes.grid(row=4, column=1)
-downalods.grid(row=5, column=1)
-views.grid(row=6, column=1)
+    return img_data
 
 
-def refresh():
-    try:
-        image['image'] = next(img_iter)
-        alt_desc['text'] = next(desc_iter)
-        username['text'] = f"Username: {next(username_iter)}"
-        downalods['text'] = f"Downloads {next(downloads_iter)}"
-        views['text'] = f"Views: {next(views_iter)}"
-        likes['text'] = f"Likes: {next(likes_iter)}"
-        window.after(2500, refresh)
-    except StopIteration:
-        pass
+class Python_Video_Downloader:
 
 
-window.after(2500, refresh)
-window.eval('tk::PlaceWindow . center')
-window.mainloop()
+
+
+    def GUI():
+        window = tk.Tk()
+        window.title("🐍 Python Youtube Downloader ▶️")
+
+        link_label = tk.Label(text="Video URL: ")
+        link_entry = tk.Entry(text="", width=40)
+        download_path_label = tk.Label(text="Download Path: ")
+        download_path = tk.Entry(text="", width=40)
+        video_title = tk.Label(text="Video Title: ")
+        select_res = ttk.Combobox(window, values=all_res)
+        select_res.current(0)
+
+        def find_video():
+            user_link = link_entry.get()
+            try:
+                yt = YouTube(user_link)
+                print(yt.title)
+                print(yt.thumbnail_url)
+                yt_thumbnail = get_url_image(yt.thumbnail_url)
+
+                im = Image.open(BytesIO(yt_thumbnail))
+                width, height = im.width, im.height
+
+                im = im.resize((int(width / 5), int(height / 5)))
+                image = ImageTk.PhotoImage(im)
+                print(image)
+
+
+                video_title['text'] = "Video Title: " + yt.title       
+                print(select_res.get())       
+
+            except Exception as e:
+                print(e)
+                messagebox.showerror("Error", f"Invalid URL link")
+
+        def download_selected_video():
+            print("Starting...")
+            yt = YouTube(f"{link_entry.get()}")
+            stream_list = yt.streams.filter(file_extension='mp4', res=f"{select_res.get()}")
+            print(stream_list)
+            for each in stream_list:
+                try:
+                    print("Downloading...")
+                    each.download(output_path=f"{download_path.get()}")
+                    print("Complete")
+                except Exception as e:
+                    print(f"[!] Error {e}")
+            print("Complete")
+
+        find_btn = tk.Button(text="Find Video", command=find_video)
+        download_btn = tk.Button(text=" Download Video", command=download_selected_video)
+
+
+        link_label.grid(row=1, column=1)
+        link_entry.grid(row=1, column=2)
+        find_btn.grid(row=1, column=3)
+        video_title.grid(row=2, column=2)
+        select_res.grid(row=2, column=3)
+        download_path_label.grid(row=4, column=1)
+        download_path.grid(row=4, column=2)
+        download_btn.grid(row=4, column=3)
+        window.mainloop()
+
+
+Python_Video_Downloader.GUI()
